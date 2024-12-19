c1db355e18065567c9fcea3bfb4dee760f44fcba
cisring <cisring@naver.com>
2024-12-19 14:28:08 +0900

    project 구조

---
+++ jeju_prj/src/main/java/kr/co/sist/JejuPrjApplication.java
@@ -0,0 +1,20 @@
+package kr.co.sist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class JejuPrjApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JejuPrjApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejuPrjApplication.class, args);
+	}
+
+}
