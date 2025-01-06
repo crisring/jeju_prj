d9d344391546652351c67f9ec50ed566789d47df
justberyllium <72927887+justberyllium@users.noreply.github.com>
2025-01-06 21:32:24 +0900

    Update SearchACMDAO.java

--- jeju_prj/src/main/java/kr/co/sist/user/searchACM/SearchACMDAO.java
+++ jeju_prj/src/main/java/kr/co/sist/user/searchACM/SearchACMDAO.java
@@ -7,34 +7,73 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import kr.co.sist.dao.MyBatisHandler;
-import kr.co.sist.user.util.SearchVO;
 
 @Repository
 public class SearchACMDAO {
-
 	/**
-	 * 숙소 유형을 가지고 검색 <br>
-	 * 1.호텔, 2. 펜션 풀빌라, 3. 게하 한옥, 4. 캠핑 글램핑, 5. 홈 빌라 <br>
+	 * SearchVO를 가지고 검색
+	 * 
+	 * @param sVO
+	 * @return
+	 */
+	public List<SearchACMDomain> selectACM(SearchVO sVO) throws PersistenceException {
+
+		List<SearchACMDomain> list = null;
+		
+		MyBatisHandler mbh = MyBatisHandler.getInstance();
+		SqlSession handler = mbh.getHandler();
+			try {
+
+				list = handler.selectOne("kr.co.sist.searchMapper.selectACM", sVO);
+
+			} finally {
+				mbh.closeHandler(handler);
+			}
+
+		return list;
+	}// selectACM
+	
+	/**
+	 * 숙소 타입 검색
 	 * 
 	 * @param acm_type_id
 	 * @return
 	 */
-	public List<SearchACMDomain> selectByACMType(int acm_type_id) throws PersistenceException {
+	public SearchACMDomain selectACMType(int acm_type_id) throws PersistenceException {
 
-		List<SearchACMDomain> list = null;
-
+		
+		SearchACMDomain sacmd = null;
 		MyBatisHandler mbh = MyBatisHandler.getInstance();
 		SqlSession handler = mbh.getHandler();
 
 		try {
-			list = handler.selectList("kr.co.sist.searchMapper.selectByACMType", acm_type_id);
+
+			sacmd = handler.selectOne("kr.co.sist.searchMapper.selectACMType", acm_type_id);
 
 		} finally {
 			mbh.closeHandler(handler);
 		}
+		return sacmd;
+	}// selectACMType
+	
+	public List<ACMTypeDomain> selectAllACMType() throws PersistenceException{
+		List<ACMTypeDomain> list=null;
+		
+		MyBatisHandler mbh = MyBatisHandler.getInstance();
+		SqlSession handler = mbh.getHandler();
 
+		try {
+
+			list = handler.selectOne("kr.co.sist.searchMapper.selectAllACMType");
+
+		} finally {
+			mbh.closeHandler(handler);
+		}
 		return list;
 	}
+
+
+	
 
 	/**
 	 * 각 숙소의 평균평점+리뷰수 계산
@@ -59,32 +98,48 @@ public class SearchACMDAO {
 
 		return sacmd;
 	}// selectRatingReview
-
+	
+	
 	/**
-	 * SearchVO를 가지고 검색
-	 * 
-	 * @param sVO
-	 * @return
-	 */
-	public List<SearchACMDomain> selectACM(SearchVO sVO) throws PersistenceException {
-
-		List<SearchACMDomain> list = null;
-
-		return list;
-	}// selectACM
-
-	/**
-	 * 숙소 타입 검색
+	 * 숙소 유형을 가지고 검색 <br>
+	 * 1.호텔, 2. 펜션 풀빌라, 3. 게하 한옥, 4. 캠핑 글램핑, 5. 홈 빌라 <br>
 	 * 
 	 * @param acm_type_id
 	 * @return
 	 */
-	public SearchACMDomain selectACMType(int acm_type_id) throws PersistenceException {
+	public List<SearchACMDomain> selectByACMType(int acm_type_id) throws PersistenceException {
 
-		SearchACMDomain sacmd = null;
+		List<SearchACMDomain> list = null;
 
-		return sacmd;
-	}// selectACMType
+		MyBatisHandler mbh = MyBatisHandler.getInstance();
+		SqlSession handler = mbh.getHandler();
+
+		try {
+			list = handler.selectList("kr.co.sist.searchMapper.selectByACMType", acm_type_id);
+
+		} finally {
+			mbh.closeHandler(handler);
+		}
+
+		return list;
+	}
+	
+	public List<FacilityDomain> selectAllFacility() throws PersistenceException{
+		List<FacilityDomain> list = null;
+
+		MyBatisHandler mbh = MyBatisHandler.getInstance();
+		SqlSession handler = mbh.getHandler();
+
+		try {
+			list = handler.selectList("kr.co.sist.searchMapper.selectAllFacility");
+
+		} finally {
+			mbh.closeHandler(handler);
+		}
+
+		return list;
+	}
+
 
 	public static void main(String[] args) {
 
