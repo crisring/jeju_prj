c2338d1ab7c03c8a0619295d41a796a7dac51e3a
justberyllium <72927887+justberyllium@users.noreply.github.com>
2025-01-06 21:24:22 +0900

    Add files via upload

---
+++ jeju_prj/src/main/java/kr/co/sist/user/searchACM/ACMTypeDomain.java
@@ -0,0 +1,12 @@
+package kr.co.sist.user.searchACM;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ACMTypeDomain {
+
+	private int acm_type_id;
+	private String acm_type;
+}
---
+++ jeju_prj/src/main/java/kr/co/sist/user/searchACM/FacilityDomain.java
@@ -0,0 +1,12 @@
+package kr.co.sist.user.searchACM;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FacilityDomain {
+
+	private int fcl_id;
+	private String fcl_name;
+}
